48b014fecab19c5bcbce1b4277b2a65460a63f0e
Aline Ferreira <alinfst@gmail.com>
2024-06-27 17:08:21 -0300

    get api data

--- app/src/main/java/com/devspacecinenow/MainActivity.kt
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -1,24 +1,76 @@
 package com.devspacecinenow
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.devspacecinenow.data.model.MovieDto
+import com.devspacecinenow.data.model.MovieResponse
+import com.devspacecinenow.data.remote.ApiService
+import com.devspacecinenow.data.remote.RetrofitClient
 import com.devspacecinenow.ui.theme.CineNowTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CineNowTheme {
-                // A surface container using the 'background' color from the theme
+                var nowPLayingMovies: List<MovieDto> by remember {
+                    mutableStateOf<List<MovieDto>>(emptyList())
+                }
+                val apiService =
+                    RetrofitClient
+                        .retrofitInstance
+                        .create(ApiService::class.java)
+
+                val callNowPlaying = apiService.getNowPlaying()
+
+                callNowPlaying.enqueue(object : Callback<MovieResponse> {
+                    override fun onResponse(
+                        call: Call<MovieResponse>,
+                        response: Response<MovieResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            val movies = response.body()?.results
+                            if (movies != null) {
+                                nowPLayingMovies = movies
+                            }
+
+                        } else {
+                            Log.d("MainActivity", "Request Error :: ${response.errorBody()}")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                        Log.d("MainActivity", "Network Error :: ${t.message}")
+                    }
+                })
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
+                    LazyColumn {
+                        items(nowPLayingMovies){
+                            Text(text = it.title)
+                            
+                        }
+
+                    }
 
                 }
             }
--- app/src/main/java/com/devspacecinenow/data/remote/ApiService.kt
+++ app/src/main/java/com/devspacecinenow/data/remote/ApiService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 
 interface ApiService {
     @GET("now_playing?language=en-US&page=1")
-    fun getAthleteListings(): Call<MovieResponse>
+    fun getNowPlaying(): Call<MovieResponse>
 }
