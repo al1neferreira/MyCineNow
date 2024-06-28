6c1769cdb37bca26d1d6756777695690f25ce685
Aline Ferreira <alinfst@gmail.com>
2024-06-28 18:51:32 -0300

    add endpoints upcoming and top rated in service

--- app/src/main/java/com/devspacecinenow/data/remote/ApiService.kt
+++ app/src/main/java/com/devspacecinenow/data/remote/ApiService.kt
@@ -7,4 +7,10 @@ import retrofit2.http.GET
 interface ApiService {
     @GET("now_playing?language=en-US&page=1")
     fun getNowPlaying(): Call<MovieResponse>
+
+    @GET("upcoming?language=en-US&page=1")
+    fun getUpcomingMovies(): Call<MovieResponse>
+
+    @GET("top_rated?language=en-US&page=1")
+    fun geTopRatedMovies(): Call<MovieResponse>
 }
