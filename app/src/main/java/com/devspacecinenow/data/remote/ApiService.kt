7a150b47344b85df53d7dfb4bc5f96f28abbe781
Aline Ferreira <alinfst@gmail.com>
2024-06-30 22:26:33 -0300

    add endpoint in api service: movie_id

--- app/src/main/java/com/devspacecinenow/data/remote/ApiService.kt
+++ app/src/main/java/com/devspacecinenow/data/remote/ApiService.kt
@@ -3,6 +3,7 @@ package com.devspacecinenow.data.remote
 import com.devspacecinenow.data.model.MovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("now_playing?language=en-US&page=1")
@@ -15,5 +16,8 @@ interface ApiService {
     fun geTopRatedMovies(): Call<MovieResponse>
 
     @GET("popular?language=en-US&page=1")
-    fun getPopularMovies():Call<MovieResponse>
+    fun getPopularMovies(): Call<MovieResponse>
+
+    @GET("movie_id?language=en-US")
+    fun getMoviesById(@Path("movie_id") movieId: String): Call<MovieResponse>
 }
